5696a8db77e35b33f35a3d328c2627bdaca0a6dc
Marcelo689 <marcelosj7159@gmail.com>
2022-09-23 10:12:54 -0300

    first commit

---
+++ CasaDoCodigo/Repositories/ItemPedidoRepository.cs
@@ -0,0 +1,30 @@
+﻿using CasaDoCodigo.Models;
+using System.Linq;
+
+namespace CasaDoCodigo.Repositories
+{
+    public interface IItemPedidoRepository
+    {
+        ItemPedido GetItem(int id);
+        void RemoveItemPedido(int itemPedidoId);
+    }
+
+    public class ItemPedidoRepository : BaseRepository<ItemPedido>, IItemPedidoRepository
+    {
+        public ItemPedidoRepository(ApplicationContext contexto) : base(contexto)
+        {
+        }
+        
+        public ItemPedido GetItem(int id)
+        {
+            var item = dbSet.Where(ip => ip.Id == id).First();
+            return item;
+        }
+
+        public void RemoveItemPedido(int itemPedidoId)
+        {
+            var itemPedido = GetItem(itemPedidoId);
+            dbSet.Remove(itemPedido);
+        }
+    }
+}
---
+++ CasaDoCodigo/wwwroot/js/Carrinho.js
@@ -0,0 +1,69 @@
+﻿class Carrinho {
+    clickIncremento(button) {
+        let data = this.getData(button);
+        data.Quantidade++;
+        this.postQuantidade(data);
+        //debugger;
+    }
+
+    clickDecremento(button) {
+        let data = this.getData(button);
+        data.Quantidade--;
+        this.postQuantidade(data);
+        //debugger;
+    }
+
+    getData(elemento) {
+        var linhaDoItem = $(elemento).parents("[item-id]");
+        var itemId = $(linhaDoItem).attr("item-id");
+        var novaQuantidade = $(linhaDoItem).find("input").val();
+
+        var data = {
+            Id: itemId,
+            Quantidade: novaQuantidade
+        };
+
+        return data;
+    }
+
+    postQuantidade(data) {
+        let token = $("[name=__RequestVerificationToken]").val();
+        let headers = {};
+
+        headers['RequestVerificationToken'] = token;
+        $.ajax({
+            url: "/pedido/updatequantidade",
+            type: "POST",
+            contentType: "application/json",
+            data: JSON.stringify(data),
+            headers:headers
+        }).done(function (response) {
+
+            var itemPedido = response.itemPedido;
+            var carrinhoViewModel = response.carrinhoViewModel;
+            var linhaDoItem = $('[item-id=' + itemPedido.id + ']');
+            linhaDoItem.find('input').val(itemPedido.quantidade);
+            linhaDoItem.find("[subtotal]").html((itemPedido.subtotal).duasCasas());
+
+            $("[numero-itens]").html("Total: " + carrinhoViewModel.itens.length + " itens");
+            var totalHTML = $("[total]");
+            var total = (carrinhoViewModel.total).duasCasas();
+            totalHTML.html(total);
+
+            if (itemPedido.quantidade == 0)
+                linhaDoItem.remove();
+        });
+    }
+
+    updateQuantidade(input) {
+        let data = this.getData(input);
+        this.postQuantidade(data);
+    }
+
+}
+
+var carrinho = new Carrinho();
+
+Number.prototype.duasCasas = function () {
+    return this.toFixed(2).replace('.',',');
+}
